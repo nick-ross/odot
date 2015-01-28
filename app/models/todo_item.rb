d875e37c01efa09cf75c20e4e7b1fa9d37ae3067
mx5433@gmail.com <mx5433@gmail.com>
2015-01-28 14:03:19 -0600

    Toggle todo item completion

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -11,4 +11,12 @@ class TodoItem < ActiveRecord::Base
 	def completed?
 		!completed_at.blank?
 	end
+
+	def toggle_completion!
+		if completed?
+			update_attribute(:completed_at, nil)
+		else
+			update_attribute(:completed_at, Time.now)
+		end
+	end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -11,4 +11,8 @@ FactoryGirl.define do
 		title "Todo List Title"
 		user
 	end
+
+	factory :todo_item do
+		content "Todo Item"
+	end
 end
